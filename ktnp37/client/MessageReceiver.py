263ae005142be8dcad0659b7f3962c9302f7b555
Dag Erik Løvgren <dagerikhl@gmail.com>
2015-03-24 12:17:09 +0100

    Laget et førsteutkast til MsgRec

--- ktnp37/client/MessageReceiver.py
+++ ktnp37/client/MessageReceiver.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from threading import Thread
+import json
+from socket import *
 
 class MessageReceiver(Thread):
     """
@@ -12,12 +14,41 @@ class MessageReceiver(Thread):
         """
         This method is executed when creating a new MessageReceiver object
         """
+        # TODO: Init the superconstructor
+        Thread.__init__(self)
 
         # Flag to run thread as a deamon
         self.daemon = True
 
         # TODO: Finish initialization of MessageReceiver
+        self.client = client
+        self.connection = connection
+        print "[+] New thread started for " + client.host + ":" + str(client.server_port)
+        self.run()
 
     def run(self):
         # TODO: Make MessageReceiver receive and handle payloads
-        pass
+        while True:
+            self.connection.listen(5)
+            payload = json.loads(connection.recv(4096))[0]
+            # TESTPRINTING
+            print payload
+            # TESTPRINTING
+            timestamp = payload["timestamp"]
+            sender = payload["sender"]
+            response = payload["response"]
+            content = payload["content"]
+
+            # Handle the different types of responses
+            message = timestamp + ": "
+            if (response == "error"):
+                message.append("[ERROR] -\t" + content)
+            elif (response == "info"):
+                message.append("[INFO] -\t" + content)
+            elif (response == "history"):
+                message.append("[HIST] -\n" + content)
+            elif (response == "message"):
+                message.append("[MSG] -\t" + sender + " says: " + content)
+            else:
+                message.append("[BAD REQUEST]")
+            self.client.receive_message(message)
